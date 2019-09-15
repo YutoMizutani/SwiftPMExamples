b6eddcc484c7a30b9687f94b34944f29f4662869
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-09-15 14:18:22 +0900

    :+1: Add include guards

--- SwiftCallC/Staticlib/Sources/CHello/include/chello.h
+++ SwiftCallC/Staticlib/Sources/CHello/include/chello.h
@@ -1,1 +1,6 @@
+#ifndef INCLUDE_CHELLO_H_
+#define INCLUDE_CHELLO_H_
+
 const char* chello();
+
+#endif /* INCLUDE_CHELLO_H_ */
--- SwiftCallCpp/CLinkage/Sources/CppHello/include/hello.h
+++ SwiftCallCpp/CLinkage/Sources/CppHello/include/hello.h
@@ -1,3 +1,6 @@
+#ifndef INCLUDE_HELLO_H_
+#define INCLUDE_HELLO_H_
+
 #ifdef __cplusplus
 extern "C"  {
 #endif
@@ -7,3 +10,5 @@ const char* hello();
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* INCLUDE_HELLO_H_ */
